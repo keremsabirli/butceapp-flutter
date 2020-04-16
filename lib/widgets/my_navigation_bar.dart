21a40c6c24c910fe1fbabfa830358b6b9293e0f8
keremsabirli <keremsabirli15@gmail.com>
2020-04-16 17:03:23 +0300

    changes

---
+++ lib/widgets/my_navigation_bar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int _currentIndex = 0;
+  onTabTapped(int index) {
+    print(index);
+    switch(index) {
+      case 0: {
+        Navigator.pushReplacementNamed(context, '/homepage');
+      }
+      break;
+      case 3: {
+        Navigator.pushReplacementNamed(context, '/profile');
+      }
+      break;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.grey[800]),
+      child: BottomNavigationBar(
+        onTap: onTabTapped,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.equalizer, color: Theme.of(context).accentColor,),
+            title: Text(
+              'Haftalık Rapor',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: Theme.of(context).accentColor
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up, color: Theme.of(context).accentColor,),
+            title: Text(
+              'Aylık Rapor',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: Theme.of(context).accentColor
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline, color: Theme.of(context).accentColor,),
+            title: Text(
+              'Tahmin',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Theme.of(context).accentColor,),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
