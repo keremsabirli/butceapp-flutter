4acb38002f1820cd42c4a245f009f0fbabcf073f
Kerem SABIRLI PEAKUP <kerem.sabirli@peakup.org>
2020-05-26 14:43:39 +0300

    report screen complete

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -13,16 +13,15 @@ class ReportScreen extends StatefulWidget {
 
 class _ReportScreenState extends State<ReportScreen> {
   String selectedReport = 'Günlük';
-  String title;
-  DateTime startDate = null;
-  DateTime endDate = null;
+  DateTime startDate = DateTime.utc(1, 1, 1);
+  DateTime endDate = DateTime.utc(2999, 12, 30);
   List<ReportData> reportData = new List<ReportData>();
   ExpenseRepository expenseRepository = new ExpenseRepository();
   List<String> _reports = <String>['Günlük', 'Aylık'];
   Future<Null> _selectStartDate(BuildContext context) async {
     final DateTime _selectStartDate = await showDatePicker(
         context: context,
-        initialDate: startDate,
+        initialDate: DateTime.now(),
         firstDate: DateTime(2018),
         lastDate: DateTime(2022),
         helpText: 'Başlangıç Tarihini Seçiniz',
@@ -31,85 +30,46 @@ class _ReportScreenState extends State<ReportScreen> {
             child: child,
           );
         }).then((date) {
-      if (date != null) {
-        setState(() {
-          this.startDate = date;
-          currentDateString();
-        });
-        print(this.startDate);
-      }
+      setState(() {
+        this.startDate = date;
+      });
+      print(this.startDate);
     });
   }
 
   Future<Null> _selectEndDate(BuildContext context) async {
     final DateTime _selectEndDate = await showDatePicker(
         context: context,
-        initialDate: endDate,
+        initialDate: DateTime.now(),
         firstDate: DateTime(2018),
         lastDate: DateTime(2022),
         helpText: 'Başlangıç Tarihini Seçiniz',
-        cancelText: '',
         builder: (context, child) {
           return SingleChildScrollView(
             child: child,
           );
         }).then((date) {
-      if (date != null) {
-        setState(() {
-          this.endDate = date;
-          currentDateString();
-        });
-        print(this.endDate);
-      }
+      setState(() {
+        this.endDate = date;
+      });
+      print(this.endDate);
     });
   }
 
   getReportData({String reportType, DateTime startDate, DateTime endDate}) {
-    if (startDate == null || endDate == null) {
-      this
-          .expenseRepository
-          .getReportData(reportType: reportType)
-          .then((value) {
-        List<ReportData> temp = new List<ReportData>();
-        value.forEach((key, value) {
-          temp.add(new ReportData(date: key, value: value));
-        });
-        setState(() {
-          this.reportData = temp;
-        });
+    this
+        .expenseRepository
+        .getReportData(
+            reportType: reportType, startDate: startDate, endDate: endDate)
+        .then((value) {
+      List<ReportData> temp = new List<ReportData>();
+      value.forEach((key, value) {
+        temp.add(new ReportData(date: key, value: value));
       });
-    } else {
-      this
-          .expenseRepository
-          .getReportData(
-              reportType: reportType, startDate: startDate, endDate: endDate)
-          .then((value) {
-        List<ReportData> temp = new List<ReportData>();
-        value.forEach((key, value) {
-          temp.add(new ReportData(date: key, value: value));
-        });
-        setState(() {
-          this.reportData = temp;
-        });
+      setState(() {
+        this.reportData = temp;
       });
-    }
-  }
-
-  currentDateString() {
-    this.title = 'Start:  ' +
-        startDate.day.toString() +
-        '/' +
-        startDate.month.toString() +
-        '/' +
-        startDate.year.toString() +
-        '    ' +
-        'End:  ' +
-        endDate.day.toString() +
-        '/' +
-        endDate.month.toString() +
-        '/' +
-        endDate.year.toString();
-    setState(() {});
+    });
   }
 
   @override
@@ -126,27 +86,25 @@ class _ReportScreenState extends State<ReportScreen> {
         children: <Widget>[
           SizedBox(height: 10),
           SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              title: ChartTitle(
-                text: this.title,
-              ),
-              series: <LineSeries<ReportData, String>>[
-                LineSeries<ReportData, String>(
-                    // Bind data source
-                    dataSource: this.reportData,
-                    xValueMapper: (ReportData sales, _) => sales.date,
-                    yValueMapper: (ReportData sales, _) => sales.value),
-              ]),
+          primaryXAxis: CategoryAxis(),
+          series: <LineSeries<ReportData, String>>[
+            LineSeries<ReportData, String>(
+                // Bind data source
+                dataSource: this.reportData,
+                xValueMapper: (ReportData sales, _) => sales.date,
+                yValueMapper: (ReportData sales, _) => sales.value),
+          ]),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              SizedBox(
-                width: 25,
-              ),
               DropdownButton(
                 onChanged: (value) {
                   setState(() {
                     this.selectedReport = value;
-                    this.getReportData(reportType: value);
+                    this.getReportData(
+                        reportType: value,
+                        startDate: startDate,
+                        endDate: endDate);
                   });
                 },
                 hint: Text('Kategori'),
@@ -158,19 +116,27 @@ class _ReportScreenState extends State<ReportScreen> {
                   );
                 }).toList(),
               ),
-              SizedBox(
-                width: 25,
-              ),
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               RaisedButton(
                 child: Text('Start Date'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).accentColor,
                 onPressed: () {
-                  this._selectStartDate(context);
-                  print(startDate);
-                  print(endDate);
-                  this.getReportData(
-                      reportType: selectedReport,
-                      startDate: startDate,
-                      endDate: endDate);
+                  setState(() {
+                    this._selectStartDate(context).then((value) {
+                      this.getReportData(
+                          reportType: selectedReport,
+                          startDate: startDate,
+                          endDate: endDate);
+                    });
+                  });
                 },
               ),
               SizedBox(
@@ -178,14 +144,35 @@ class _ReportScreenState extends State<ReportScreen> {
               ),
               RaisedButton(
                 child: Text('End Date'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).accentColor,
                 onPressed: () {
-                  this._selectEndDate(context);
-                  print(startDate);
-                  print(endDate);
-                  this.getReportData(
-                      reportType: selectedReport,
-                      startDate: startDate,
-                      endDate: endDate);
+                  setState(() {
+                    this._selectEndDate(context).then((value) {
+                      this.getReportData(
+                          reportType: selectedReport,
+                          startDate: startDate,
+                          endDate: endDate);
+                    });
+                  });
+                },
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              RaisedButton(
+                child: Text('Reset'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).accentColor,
+                onPressed: () {
+                  setState(() {
+                    this.startDate = DateTime.utc(1,1,1);
+                    this.endDate = DateTime.utc(9000,1,1);
+                    this.getReportData(
+                          reportType: selectedReport,
+                          startDate: startDate,
+                          endDate: endDate);
+                  });
                 },
               ),
             ],
